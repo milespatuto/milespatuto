e75458930c1c5e19938fe41439bc2d87478f73d6
milespatuto <milespatuto@gmail.com>
2024-10-20 17:38:07 +0000

    sss

---
+++ quiz.py
@@ -0,0 +1,12 @@
+print("Hard Quiz")
+x=float(input ("What is 3x4+2/4"))
+if(x== 12.5):
+    print("Correct")
+else:
+    print("INCORRECT")
+ 
+y=float(input ("What is the square root of 12"))
+if(y== 3.46410162):
+    print("Correct")
+else:
+    print("INCORRECT")   
